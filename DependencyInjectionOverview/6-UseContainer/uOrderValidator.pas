7d945ded038e2e6512e5db784a2a6fbbcbf58a73
Jeroen Wiert Pluimers <jeroen.bitbucket.com@pluimers.com>
2013-10-16 09:00:19 +0200

    Moved TOrderEntry and TOrderValidator to the interface sections as we need them to validate the object instances behind the IOrderEntry and IOrderValidator references to be of the correct class in the unit tests.

--- DependencyInjectionOverview/6-UseContainer/uOrderValidator.pas
+++ DependencyInjectionOverview/6-UseContainer/uOrderValidator.pas
@@ -2,21 +2,24 @@ unit uOrderValidator;
 
 interface
 
-implementation
-
 uses
   uOrder,
-  uOrderInterfaces,
-  Spring.Container;
+  uOrderInterfaces;
 
 type
   // Moving the `TOrderValidator` to the `implementation` section might sound like a smart idea, but it is not:
   // The only way to ever reach `TOrderValidator` reach is through the `GlobalContainer` (using `ServiceLocator`).
   // So you now are coupled to the DI framework which contradicts what we want to do: decoupling.
+  // Moving it to the `implementation` section makes writing test cases a lot harder as you have to go through the DI framework.
   TOrderValidator = class(TInterfacedObject, IOrderValidator)
   public
     function ValidateOrder(const aOrder: TOrder): Boolean;
   end;
+
+implementation
+
+uses
+  Spring.Container;
 
 function TOrderValidator.ValidateOrder(const aOrder: TOrder): Boolean;
 begin
